b09760ef2b66860e9d7ad1138ebf4c14b0340c89
Ridwan Sharkar <ridwansharkar@gmail.com>
2024-08-19 18:06:21 -0400

    scheduled scraper

---
+++ src/main/java/com/rsharkar/Arbitrager/ScheduledScrape.java
@@ -0,0 +1,20 @@
+/* X src/main/java\com\rsharkar\arbitrager/ ScheduledScrape.java X */
+package com.rsharkar.Arbitrager;
+
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ScheduledScrape {
+
+    private final FightOddsScraper fightOddsScraper;
+
+    public ScheduledScrape(FightOddsScraper fightOddsScraper) {
+        this.fightOddsScraper = fightOddsScraper;
+    }
+
+    @Scheduled(fixedRate = 3600000) // Scrape odds every hour
+    public void performScraping() {
+        fightOddsScraper.scrapeOdds(); 
+    }
+}
